4acc33e347da2736dd89ce214c07dac6d7533282
madisonlowe <mcourteney@live.com>
2022-07-17 15:55:39 +0100

    Updated App component starting state data.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -20,39 +20,12 @@ function App() {
       lon: -0.1257,
       country: "GB",
     },
-    {
-      name: "London",
-      lat: 42.9834,
-      lon: -81.233,
-      country: "CA",
-    },
-    {
-      name: "London",
-      lat: 39.8865,
-      lon: -83.4483,
-      country: "US",
-      state: "OH",
-    },
-    {
-      name: "London",
-      lat: 37.129,
-      lon: -84.0833,
-      country: "US",
-      state: "KY",
-    },
-    {
-      name: "London",
-      lat: 36.4761,
-      lon: -119.4432,
-      country: "US",
-      state: "CA",
-    },
   ]);
   const [latlon, setLatlon] = useState<number[]>([51.5085, -0.1257]);
 
   return (
     <main className="App">
-      <h1>this is our weather app tee hee</h1>
+      <h1>Weather App</h1>
       <Input setGeolocatorData={setGeolocatorData} />
       <Geolocator geolocatorData={geolocatorData} setLatlon={setLatlon} />
       <Weather latlon={latlon} />
